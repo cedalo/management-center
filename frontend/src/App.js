8d9c1e2380ae6c4032198162a4231d5d4589846b
Philip Ackermann <info@philipackermann.de>
2020-08-14 17:15:21 +0200

    Add routing and diifferent skeleton pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,78 +16,109 @@ import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import GroupIcon from '@material-ui/icons/Group';
-import PersonIcon from '@material-ui/icons/Person';
-import PolicyIcon from '@material-ui/icons/Policy';
-import EqualizerIcon from '@material-ui/icons/Equalizer';
-import SettingsIcon from '@material-ui/icons/Settings';
+import GroupIcon from "@material-ui/icons/Group";
+import PersonIcon from "@material-ui/icons/Person";
+import PolicyIcon from "@material-ui/icons/Policy";
+import EqualizerIcon from "@material-ui/icons/Equalizer";
+import SettingsIcon from "@material-ui/icons/Settings";
+import Container from "@material-ui/core/Container";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from "react-router-dom";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-	  display: 'flex',
-	},
-	appBar: {
-	  zIndex: theme.zIndex.drawer + 1,
-	  transition: theme.transitions.create(['width', 'margin'], {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.leavingScreen,
-	  }),
-	},
-	appBarShift: {
-	  marginLeft: drawerWidth,
-	  width: `calc(100% - ${drawerWidth}px)`,
-	  transition: theme.transitions.create(['width', 'margin'], {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.enteringScreen,
-	  }),
-	},
-	menuButton: {
-	  marginRight: 36,
-	},
-	hide: {
-	  display: 'none',
-	},
-	drawer: {
-	  width: drawerWidth,
-	  flexShrink: 0,
-	  whiteSpace: 'nowrap',
-	},
-	drawerOpen: {
-	  width: drawerWidth,
-	  transition: theme.transitions.create('width', {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.enteringScreen,
-	  }),
-	},
-	drawerClose: {
-	  transition: theme.transitions.create('width', {
-		easing: theme.transitions.easing.sharp,
-		duration: theme.transitions.duration.leavingScreen,
-	  }),
-	  overflowX: 'hidden',
-	  width: theme.spacing(7) + 1,
-	  [theme.breakpoints.up('sm')]: {
-		width: theme.spacing(9) + 1,
-	  },
-	},
-	toolbar: {
-	  display: 'flex',
-	  alignItems: 'center',
-	  justifyContent: 'flex-end',
-	  padding: theme.spacing(0, 1),
-	  // necessary for content to be below app bar
-	  ...theme.mixins.toolbar,
-	},
-	content: {
-	  flexGrow: 1,
-	  padding: theme.spacing(3),
-	},
-  }));
+  root: {
+    display: "flex",
+  },
+  container: {
+    paddingTop: "100px",
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: "nowrap",
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+function ListItemLink(props) {
+  const { icon, primary, to } = props;
+
+  const renderLink = React.useMemo(
+    () =>
+      React.forwardRef((itemProps, ref) => (
+        <RouterLink to={to} ref={ref} {...itemProps} />
+      )),
+    [to]
+  );
+
+  return (
+    <li>
+      <ListItem button component={renderLink}>
+        {icon ? <ListItemIcon>{icon}</ListItemIcon> : null}
+        <ListItemText primary={primary} />
+      </ListItem>
+    </li>
+  );
+}
 
 export default function App() {
-    const classes = useStyles();
+  const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
@@ -100,90 +131,88 @@ export default function App() {
   };
 
   return (
-    <div className={classes.root}>
-		<CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap>
-            Mosquitto UI
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
-        classes={{
-          paper: clsx({
+    <Router>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+          })}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+              })}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              Mosquitto UI
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          className={clsx(classes.drawer, {
             [classes.drawerOpen]: open,
             [classes.drawerClose]: !open,
-          }),
-        }}
-      >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </div>
-        <Divider />
-        <List>
-			<ListItem button key={"Users"}>
-				<ListItemIcon>
-				<PersonIcon />
-				</ListItemIcon>
-				<ListItemText primary={"Users"} />
-			</ListItem>
-			<ListItem button key={"Groups"}>
-				<ListItemIcon>
-				<GroupIcon />
-				</ListItemIcon>
-				<ListItemText primary={"Groups"} />
-			</ListItem>
-			<ListItem button key={"Policies"}>
-				<ListItemIcon>
-				<PolicyIcon />
-				</ListItemIcon>
-				<ListItemText primary={"Policies"} />
-			</ListItem>
-        </List>
-        <Divider />
-        <List>
-			<ListItem button key={"System Status"}>
-				<ListItemIcon>
-				<EqualizerIcon />
-				</ListItemIcon>
-				<ListItemText primary={"System Status"} />
-			</ListItem>
-			<ListItem button key={"Settings"}>
-				<ListItemIcon>
-				<SettingsIcon />
-				</ListItemIcon>
-				<ListItemText primary={"Settings"} />
-			</ListItem>
-        </List>
-      </Drawer>
-    </div>
+          })}
+          classes={{
+            paper: clsx({
+              [classes.drawerOpen]: open,
+              [classes.drawerClose]: !open,
+            }),
+          }}
+        >
+          <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "rtl" ? (
+                <ChevronRightIcon />
+              ) : (
+                <ChevronLeftIcon />
+              )}
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <ListItemLink to="/users" primary="Users" icon={<PersonIcon />} />
+            <ListItemLink to="/groups" primary="Groups" icon={<GroupIcon />} />
+            <ListItemLink
+              to="/policies"
+              primary="Policies"
+              icon={<PolicyIcon />}
+            />
+          </List>
+          <Divider />
+          <List>
+            <ListItemLink
+              to="/system"
+              primary="System Status"
+              icon={<EqualizerIcon />}
+            />
+            <ListItemLink
+              to="/settings"
+              primary="Settings"
+              icon={<SettingsIcon />}
+            />
+          </List>
+        </Drawer>
+        <Container className={classes.container}>
+          <Switch>
+            <Route path="/users">Users</Route>
+            <Route path="/groups">Groups</Route>
+            <Route path="/policies">Policies</Route>
+            <Route path="/system">System</Route>
+            <Route path="/settings">Settings</Route>
+          </Switch>
+        </Container>
+      </div>
+    </Router>
   );
 }
