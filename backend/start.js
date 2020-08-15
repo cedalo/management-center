3def61e73237ebed51660af2a03a8b1593c7c4c8
Philip Ackermann <info@philipackermann.de>
2020-08-15 15:22:24 +0200

    Build and populate JSON object from system topics

--- backend/start.js
+++ backend/start.js
@@ -11,3 +11,27 @@ client.on("connect", () => {
     }
   });
 });
+
+const system = {};
+
+const updateSystemTopics = (system, topic, message) => {
+  const parts = topic.split("/");
+  let current = system;
+  parts.forEach((part, index) => {
+    if (!current[part]) {
+      current[part] = {};
+    }
+    if (index + 1 === parts.length) {
+      current[part] = message.toString();
+    }
+    current = current[part];
+  });
+  return system;
+};
+
+client.on("message", (topic, message) => {
+  updateSystemTopics(system, topic, message);
+  console.log(JSON.stringify(system, null, 2));
+  console.log(system.$SYS?.broker?.clients?.connected);
+  client.end();
+});
