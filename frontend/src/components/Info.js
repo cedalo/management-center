ca5cd61463039da49aaff91ccd941b7c32b7b2c7
Philip Ackermann <info@philipackermann.de>
2020-08-17 14:08:03 +0200

    Add info component for displaying single broker status information

---
+++ frontend/src/components/Info.js
@@ -0,0 +1,19 @@
+        {/* <Box
+          mt={2}
+          display="flex"
+          alignItems="center"
+        >
+          <ArrowDownwardIcon className={classes.differenceIcon} />
+          <Typography
+            className={classes.differenceValue}
+            variant="body2"
+          >
+            12%
+          </Typography>
+          <Typography
+            color="textSecondary"
+            variant="caption"
+          >
+            Since last month
+          </Typography>
+        </Box> */}
