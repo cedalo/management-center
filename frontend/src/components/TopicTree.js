4eb609e02ef50e86b2b172d7b770fca8ba27e88b
Philip Ackermann <info@philipackermann.de>
2020-08-18 19:35:22 +0200

    Add component for displaying the MQTT topic tree

---
+++ frontend/src/components/TopicTree.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { connect } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import TreeItem from "@material-ui/lab/TreeItem";
+
+const useStyles = makeStyles({
+  root: {
+    height: 216,
+    flexGrow: 1,
+    maxWidth: 400,
+  },
+});
+
+const generateTreeData = (id, name, obj) => {
+  const node = {
+    id,
+    name,
+    children: [],
+  };
+  const properties = Object.keys(obj);
+  if (properties.length > 0) {
+    properties.forEach((property) => {
+      node.children.push(generateTreeData(property, property, obj[property]));
+    });
+  }
+  return node;
+};
+
+const TopicTree = ({ topicTree }) => {
+  const classes = useStyles();
+
+  console.log("topicTree");
+  console.log(topicTree);
+
+  const data = generateTreeData("Topic Tree", "Topic Tree", topicTree);
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
+  return (
+    <TreeView
+      className={classes.root}
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {renderTree(data)}
+    </TreeView>
+  );
+};
+
+const mapStateToProps = (state) => {
+  console.log("state");
+  console.log(state.topicTree);
+  return {
+    // TODO: check object hierarchy
+    topicTree: state.topicTree,
+  };
+};
+
+export default connect(mapStateToProps)(TopicTree);
