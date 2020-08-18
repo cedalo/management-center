74fe51c36d64a3a5cf397b97ba86ff5c09ece219
Philip Ackermann <info@philipackermann.de>
2020-08-18 19:40:22 +0200

    Add info component

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -1,3 +1,69 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import {
+  Avatar,
+  Box,
+  Card,
+  CardContent,
+  Grid,
+  Typography,
+  colors,
+  makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100%'
+  },
+  avatar: {
+    backgroundColor: colors.green[600],
+    height: 56,
+    width: 56
+  },
+  differenceIcon: {
+    color: colors.red[900]
+  },
+  differenceValue: {
+    color: colors.red[900],
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const Info = ({ className, label, value, icon, ...rest }) => {
+  const classes = useStyles();
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardContent>
+        <Grid
+          container
+          justify="space-between"
+          spacing={3}
+        >
+          <Grid item>
+            <Typography
+              color="textSecondary"
+              gutterBottom
+            >
+              {label}
+            </Typography>
+            <Typography
+              color="textPrimary"
+              variant="h5"
+            >
+              {value}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              {icon}
+            </Avatar>
+          </Grid>
+        </Grid>
         {/* <Box
           mt={2}
           display="flex"
@@ -17,3 +83,13 @@
             Since last month
           </Typography>
         </Box> */}
+      </CardContent>
+    </Card>
+  );
+};
+
+Info.propTypes = {
+  className: PropTypes.string
+};
+
+export default Info;
