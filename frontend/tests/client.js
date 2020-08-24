e67c87f3a89bf9219c28341092265df192b15330
Philip Ackermann <info@philipackermann.de>
2020-08-24 16:25:20 +0200

    Update Node.js based client example to handle Mosquitto proxy events

--- frontend/tests/client.js
+++ frontend/tests/client.js
@@ -7,6 +7,12 @@ const MOSQUITTO_PROXY_PORT = process.env.MOSQUITTO_PROXY_PORT || 8088;
 	const client = new NodeMosquittoClient({ logger: console });
 	try {
 		await client.connect({ socketEndpointURL: `${MOSQUITTO_PROXY_URL}:${MOSQUITTO_PROXY_PORT}` });
+		client.on('system_status', (message) => {
+			console.log(message);
+		});
+		client.on('topic_tree', (message) => {
+			console.log(message);
+		});
 		const addUserResponse = await client.addUser('maxmustermann', 'secret', '1234567');
 		const assGroupResponse = await client.addGroup('default');
 	} catch (error) {
