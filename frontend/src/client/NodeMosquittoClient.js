bf266e05a2fa2c23d36e90319bf3bae9ba5189a0
Philip Ackermann <info@philipackermann.de>
2020-08-24 16:05:55 +0200

    Add Node.js based Mosquitto client

---
+++ frontend/src/client/NodeMosquittoClient.js
@@ -0,0 +1,18 @@
+const WebSocket = require('ws');
+const BaseMosquittoClient = require('./BaseMosquittoClient');
+
+module.exports = class NodeMosquittoClient extends BaseMosquittoClient {
+	constructor({ name = 'Node Mosquitto Client', logger } = {}) {
+		super({ name, logger: logger || console });
+	}
+
+	_connectSocketServer(url) {
+		return new Promise((resolve) => {
+			const ws = new WebSocket(url);
+			ws.on('open', () => this._handleOpenedSocketConnection().then(() => resolve(ws)));
+			ws.on('message', message => this._handleSocketMessage(message));
+			ws.on('error', event => this._handleSocketError(event));
+			ws.on('close', event => this._handleSocketClose(event));
+		});
+	}
+};
