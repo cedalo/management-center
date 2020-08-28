1aac809dc7db765ffb80eb68299dc5ea70fd0b0a
Philip Ackermann <info@philipackermann.de>
2020-08-28 20:23:49 +0200

    Add dummy data for groups

---
+++ frontend/src/data/groups.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		"groupname": "admins",
+		"policyName": "",
+		"users": [
+			"maxmustermann", "maxmustermann2"
+		]
+	},
+	{
+		"groupname": "example1",
+		"policyName": "",
+		"users": [
+			"maxmustermann", "maxmustermann2"
+		]
+	},
+	{
+		"groupname": "example2",
+		"policyName": "",
+		"users": [
+			"maxmustermann", "maxmustermann2"
+		]
+	}
+]
