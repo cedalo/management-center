4ed6472f889e33dc134dda72d62f44c701b72a52
Philip Ackermann <info@philipackermann.de>
2020-08-28 20:23:26 +0200

    Add logo component

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Typography from "@material-ui/core/Typography";
+
+function Logo() {
+  return (
+      <Typography variant="h6" noWrap>
+        powered by Cedalo AG
+      </Typography>
+  );
+}
+
+export default Logo;
