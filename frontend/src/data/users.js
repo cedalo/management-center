d86b4d320ab16334c2736a87fd906c36e3021eab
Philip Ackermann <info@philipackermann.de>
2020-08-28 20:23:41 +0200

    Add dummy data for users

---
+++ frontend/src/data/users.js
@@ -0,0 +1,73 @@
+export default [
+	{
+		"clientid": "1",
+		"username": "maxmustermann",
+		"firstName": "Max",
+		"lastName": "Mustermann",
+		"password": "",
+		"banned": false,
+		"connectionRate": 0,
+		"messageRate": 0,
+		"policyName": "",
+		"groups": [
+			{
+				"name": "admins",
+				"priority": 0
+			},
+			{
+				"name": "example",
+				"priority": 0
+			},
+		]
+	},
+	{
+		"clientid": "2",
+		"username": "maxmustermann2",
+		"firstName": "Max",
+		"lastName": "Mustermann",
+		"password": "",
+		"banned": false,
+		"connectionRate": 0,
+		"messageRate": 0,
+		"policyName": "",
+		"groups": [
+			{
+				"name": "admins",
+				"priority": 0
+			},
+			{
+				"name": "example",
+				"priority": 0
+			},
+			{
+				"name": "example2",
+				"priority": 0
+			},
+		]
+	},
+	{
+		"clientid": "3",
+		"username": "maxmustermann3",
+		"firstName": "Max",
+		"lastName": "Mustermann",
+		"password": "",
+		"banned": false,
+		"connectionRate": 0,
+		"messageRate": 0,
+		"policyName": "",
+		"groups": [
+			{
+				"name": "example",
+				"priority": 0
+			},
+			{
+				"name": "example2",
+				"priority": 0
+			},
+			{
+				"name": "example3",
+				"priority": 0
+			},
+		]
+	},
+]
