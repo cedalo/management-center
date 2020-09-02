501207f6b81ac363767c7d46ac29a74293e17c75
Philip Ackermann <info@philipackermann.de>
2020-09-02 16:34:32 +0200

    Only set top margin for user detail tab box

--- frontend/src/components/UserDetail.js
+++ frontend/src/components/UserDetail.js
@@ -28,7 +28,7 @@ function TabPanel(props) {
       {...other}
     >
       {value === index && (
-        <Box p={3}>
+        <Box pt={3}>
           <Typography>{children}</Typography>
         </Box>
       )}
