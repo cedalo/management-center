e9338442412b24759d10ace706dfc0a0770f02d9
Philip Ackermann <info@philipackermann.de>
2020-09-07 17:38:31 +0200

    Add script for starting Mosquitto UI and Mosquitto Proxy

---
+++ mosquitto-ui.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+
+echo -e "${GREEN}Starting Mosquitto UI${NOCOLOR}"
+
+node frontend/start.js &
+node backend/start.js
