d1d87c9f025c5040b484dd740b0270b6968dc938
Philip Ackermann <info@philipackermann.de>
2020-09-14 16:39:01 +0200

    Integrate users reducer in store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,12 +4,14 @@ import brokerConfigurationsReducer from './reducers/brokerConfigurationsReducer'
 import brokerConnectionsReducer from './reducers/brokerConnectionsReducer';
 import systemStatusReducer from './reducers/systemStatusReducer';
 import topicTreeReducer from './reducers/topicTreeReducer';
+import usersReducer from './reducers/usersReducer';
 
 const store = createStore(combineReducers({
 	brokerConfigurations: brokerConfigurationsReducer,
 	brokerConnections: brokerConnectionsReducer,
 	systemStatus: systemStatusReducer,
-	topicTree: topicTreeReducer
+	topicTree: topicTreeReducer,
+	users: usersReducer,
 }));
 
 export default store;
