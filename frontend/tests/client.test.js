adc096c2d84f1b45be6a574d6537c54350b94e61
Philip Ackermann <info@philipackermann.de>
2020-09-16 16:33:32 +0200

    Add Mosquitto proxy client test for addUser()

---
+++ frontend/tests/client.test.js
@@ -0,0 +1,26 @@
+
+const NodeMosquittoProxyClient = require('../src/client/NodeMosquittoProxyClient');
+
+const MOSQUITTO_PROXY_URL = process.env.MOSQUITTO_PROXY_URL || 'ws://localhost';
+const MOSQUITTO_PROXY_PORT = process.env.MOSQUITTO_PROXY_PORT || 8088;
+
+const client = new NodeMosquittoProxyClient({ /* logger: console */ });
+const TEST_BROKER = 'Mosquitto 2.0 Mock API';
+
+beforeAll(async (callback) => {
+	await client.connect({ socketEndpointURL: `${MOSQUITTO_PROXY_URL}:${MOSQUITTO_PROXY_PORT}` });
+	await client.connectToBroker(TEST_BROKER);
+	callback();
+});
+
+afterAll(async (callback) => {
+	await client.disconnectFromBroker(TEST_BROKER);
+	callback();
+});
+test('addUser()', async (callback) => {
+	const initialUsers = await client.getUserCount();
+	await client.addUser('maxmustermann', 'secret', 'fsdf');
+	const newUsers = await client.getUserCount();
+	expect(newUsers).toBe(initialUsers + 1);
+	callback();
+});
