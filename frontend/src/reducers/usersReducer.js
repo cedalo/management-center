ac1acf38f90c6459630a9eb4e8e8c931e142eccc
Philip Ackermann <info@philipackermann.de>
2020-09-17 17:00:51 +0200

    Remove console output

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -1,8 +1,6 @@
 import * as ActionTypes from '../constants/ActionTypes';
 
 export default function users(state = {}, action) {
-	console.log("action");
-	console.log(action.update)
 	const newState = { ...state };
 	switch (action.type) {
 		case ActionTypes.UPDATE_USERS:
