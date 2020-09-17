d87805fddadaf0628248696c054e4fb601b16e98
Philip Ackermann <info@philipackermann.de>
2020-09-17 17:01:07 +0200

    Add groups reducer

---
+++ frontend/src/reducers/groupsReducer.js
@@ -0,0 +1,12 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+export default function groups(state = {}, action) {
+	const newState = { ...state };
+	switch (action.type) {
+		case ActionTypes.UPDATE_GROUPS:
+			newState.groups = action.update;
+			break;
+		default:
+	}
+	return newState;
+}
