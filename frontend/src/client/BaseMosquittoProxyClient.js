689ee618b1de941c41cb8097ff0d671f421d31de
Philip Ackermann <info@philipackermann.de>
2020-09-17 17:02:43 +0200

    Add client method for deleting all groups

--- frontend/src/client/BaseMosquittoProxyClient.js
+++ frontend/src/client/BaseMosquittoProxyClient.js
@@ -371,6 +371,13 @@ export default class BaseMosquittoProxyClient {
 		}
 	}
 
+	async deleteAllGroups() {
+		const groups = await this.listGroups();
+		for (const group of groups) {
+			await this.deleteGroup(group.groupname);
+		}
+	}
+
 	async deleteAll() {
 		await this.deleteAllUsers();
 	}
