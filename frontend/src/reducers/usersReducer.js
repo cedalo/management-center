ca561ff4508ab1ab614e20e97baeb74c4c19821d
Philip Ackermann <info@philipackermann.de>
2020-09-22 19:46:26 +0200

    Add reducer for updating current user

--- frontend/src/reducers/usersReducer.js
+++ frontend/src/reducers/usersReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../constants/ActionTypes';
 export default function users(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_USER:
+			newState.user = action.update;
+			break;
 		case ActionTypes.UPDATE_USERS:
 			newState.users = action.update;
 			break;
