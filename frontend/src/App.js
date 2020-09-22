c669647c9d6451bff57c771309d2bfb3101728b5
Philip Ackermann <info@philipackermann.de>
2020-09-22 19:51:19 +0200

    Use group id in React route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -350,9 +350,7 @@ export default function App(props) {
                     <Route path="/security/users">
                       <Users />
                     </Route>
-                    <Route path="/security/groups/detail">
-                      <GroupDetail />
-                    </Route>
+                    <Route path="/security/groups/:groupId" component={GroupDetail} />
                     <Route path="/security/groups">
                       <Groups />
                     </Route>
