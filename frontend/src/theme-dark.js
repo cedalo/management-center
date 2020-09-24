d4eaa4a1d5bbcee0770c9273d4e0b36487b58100
Philip Ackermann <info@philipackermann.de>
2020-09-24 09:02:10 +0200

    Add theme for dark mode

---
+++ frontend/src/theme-dark.js
@@ -0,0 +1,38 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+	type: "dark",
+    primary: {
+	  main: '#556cd6',
+	//   main: '#00695f',
+	//   main: '#ffc107',
+	//   main: 'rgb(44, 19, 56)',
+    },
+    secondary: {
+      main: '#7c88cc',
+    //   main: '#009688',
+    //   main: '#ffcd38',
+    },
+    error: {
+      main: red.A400,
+    },
+	breadcrumbItem: {
+		fontSize: "12px",
+		fontWeight: '500',
+		textTransform: "uppercase",
+	},
+	breadcrumbLink: {
+		color: "inherit",
+		textDecoration: "none",
+		textTransform: "uppercase",
+		fontSize: "12px",
+		"&:hover": {
+			textDecoration: "underline"
+		}
+	}
+  }
+});
+
+export default theme;
