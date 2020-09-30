d93e656b03e431bec6c2dff5ddb9e9ed3f80910d
Philip Ackermann <info@philipackermann.de>
2020-09-30 08:12:00 +0200

    Add confirm provider for generic confirm dialogs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ConfirmProvider } from 'material-ui-confirm';
+// import CssBaseline from '@material-ui/core/CssBaseline';
+// import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
-import theme from './theme';
+// import theme from './theme';
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+	<ConfirmProvider>
+		<App />
+	</ConfirmProvider>
+  ,
   document.querySelector('#root'),
 );
