de25a120d78f40cb6d06dc704ffd8de3a47fb713
Philip Ackermann <info@philipackermann.de>
2020-10-02 15:40:35 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Badge from '@material-ui/core/Badge';
 import Button from "@material-ui/core/Button";
 import MenuIcon from "@material-ui/icons/Menu";
 import Drawer from "@material-ui/core/Drawer";
@@ -284,9 +285,9 @@ export default function App(props) {
 	<ThemeProvider theme={appliedTheme}>
 	  <CssBaseline />
     <Router>
-      <Provider store={store}>
+      <Provider store={store} >
         <WebSocketProvider>
-          <div className={classes.root}>
+          <div className={classes.root} >
             <Switch>
               <Route path="/login">
 
