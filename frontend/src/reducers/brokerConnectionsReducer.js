9921044ccff87113577ae719d633ca5f8727cd3d
Philip Ackermann <info@philipackermann.de>
2020-10-06 19:56:41 +0200

    Handle broker connectivity state in reducer

--- frontend/src/reducers/brokerConnectionsReducer.js
+++ frontend/src/reducers/brokerConnectionsReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../constants/ActionTypes';
 export default function brokerConnections(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_BROKER_CONNECTED:
+			newState.connected = action.connected;
+			break;
 		case ActionTypes.UPDATE_BROKER_CONNECTIONS:
 			newState.brokerConnections = action.update;
 			break;
