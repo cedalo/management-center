28eaf66ac2856e8d5e47b208a2b5439e48459716
Philip Ackermann <info@philipackermann.de>
2020-10-06 19:49:09 +0200

    Add roles reducer

---
+++ frontend/src/reducers/rolesReducer.js
@@ -0,0 +1,15 @@
+import * as ActionTypes from '../constants/ActionTypes';
+
+export default function roles(state = {}, action) {
+	const newState = { ...state };
+	switch (action.type) {
+		case ActionTypes.UPDATE_ROLE:
+			newState.role = action.update;
+			break;
+		case ActionTypes.UPDATE_ROLES:
+			newState.roles = action.update;
+			break;
+		default:
+	}
+	return newState;
+}
