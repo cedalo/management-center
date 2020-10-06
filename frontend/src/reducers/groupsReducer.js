e2ff4b966209fcdeba843682c70fb388156cf92d
Philip Ackermann <info@philipackermann.de>
2020-10-06 19:49:28 +0200

    Handle update of current group

--- frontend/src/reducers/groupsReducer.js
+++ frontend/src/reducers/groupsReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../constants/ActionTypes';
 export default function groups(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_GROUP:
+			newState.group = action.update;
+			break;
 		case ActionTypes.UPDATE_GROUPS:
 			newState.groups = action.update;
 			break;
