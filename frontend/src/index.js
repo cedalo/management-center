4d587e803bba0e4cf725079332c92ec1c5f5e2cb
Philip Ackermann <info@philipackermann.de>
2020-10-09 18:12:55 +0200

    Fix dark mode display error of confirm dialog by moving confirm provider into app component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ConfirmProvider } from 'material-ui-confirm';
+
 // import CssBaseline from '@material-ui/core/CssBaseline';
 // import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 // import theme from './theme';
 
 ReactDOM.render(
-	<ConfirmProvider>
 		<App />
-	</ConfirmProvider>
   ,
   document.querySelector('#root'),
 );
