4ccf3c0da1f30af62d03cc5a8d129240f5a17f51
Philip Ackermann <info@philipackermann.de>
2020-10-12 11:08:33 +0200

    Remove console logging

--- frontend/src/reducers/brokerConfigurationsReducer.js
+++ frontend/src/reducers/brokerConfigurationsReducer.js
@@ -1,8 +1,6 @@
 import * as ActionTypes from '../constants/ActionTypes';
 
 export default function brokerConfigurations(state = {}, action) {
-	console.log("action");
-	console.log(action.update)
 	const newState = { ...state };
 	switch (action.type) {
 		case ActionTypes.UPDATE_BROKER_CONFIGURATIONS:
