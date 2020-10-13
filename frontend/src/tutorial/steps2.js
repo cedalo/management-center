9fc9e632cc359deca511ecc5658214cd3d1593b2
Philip Ackermann <info@philipackermann.de>
2020-10-13 14:41:22 +0200

    Add steps for in-app tour

---
+++ frontend/src/tutorial/steps2.js
@@ -0,0 +1,27 @@
+export default
+	[
+		{
+		  target: '#connection-select',
+		  content: 'Select your Mosquitto broker here',
+		},
+		{
+		  target: '#menu-item-clients',
+		  content: 'Create and manage clients.',
+		},
+		{
+		  target: '#menu-item-groups',
+		  content: 'Create and manage groups.',
+		},
+		{
+		  target: '#menu-item-roles',
+		  content: 'Create and manage roles and add them to clients and groups.',
+		},
+		{
+		  target: '#menu-item-status',
+		  content: 'Monitor the status of your Mosquitto broker.',
+		},
+		{
+		  target: '#menu-item-topics',
+		  content: 'Analyse all topics with this awesome topic tree.',
+		},
+	];
