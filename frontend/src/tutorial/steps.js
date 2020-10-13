84de3b2167900bc8ec9a59b5fd064ac72304f0d9
Philip Ackermann <info@philipackermann.de>
2020-10-13 14:44:34 +0200

    Update steps for in-app tour

--- frontend/src/tutorial/steps.js
+++ frontend/src/tutorial/steps.js
@@ -1,190 +1,27 @@
-export default [
-	{
-	  id: 'intro',
-	  attachTo: {
-		  element: '#menu-item-home',
-		  on: 'right-start'
-	  },
-	  beforeShowPromise: function () {
-		return new Promise(function (resolve) {
-		  setTimeout(function () {
-			window.scrollTo(0, 0);
-			resolve();
-		  }, 500);
-		});
-	  },
-	  buttons: [
+export default
+	[
 		{
-		  classes: 'tour-button-secondary',
-		  text: 'Exit',
-		  type: 'cancel'
+		  target: '#connection-select',
+		  content: 'Select your Mosquitto broker here',
 		},
 		{
-		  classes: 'tour-button-primary',
-		  text: 'Back',
-		  type: 'back'
+		  target: '#menu-item-clients',
+		  content: 'Create and manage clients.',
 		},
 		{
-		  classes: 'tour-button-primary',
-		  text: 'Next',
-		  type: 'next'
-		}
-	  ],
-	  highlightClass: 'highlight',
-	  scrollTo: false,
-	  cancelIcon: {
-		enabled: true,
-	  },
-	  title: 'Welcome to React-Shepherd!',
-	  text: ['React-Shepherd is a JavaScript library for guiding users through your React app.'],
-	  when: {
-		show: () => {
-		  console.log('show step');
-		},
-		hide: () => {
-		  console.log('hide step');
-		}
-	  }
-	},
-	{
-	  id: 'intro2',
-	  attachTo: {
-		element: '#menu-item-clients',
-		on: 'right-start'
-	  },
-	  beforeShowPromise: function () {
-		return new Promise(function (resolve) {
-		  setTimeout(function () {
-			window.scrollTo(0, 0);
-			resolve();
-		  }, 500);
-		});
-	  },
-	  buttons: [
-		{
-		  classes: 'tour-button-secondary',
-		  text: 'Exit',
-		  type: 'cancel'
+		  target: '#menu-item-groups',
+		  content: 'Create and manage groups.',
 		},
 		{
-		  classes: 'tour-button-primary',
-		  text: 'Back',
-		  type: 'back'
+		  target: '#menu-item-roles',
+		  content: 'Create and manage roles and add them to clients and groups.',
 		},
 		{
-		  classes: 'tour-button-primary',
-		  text: 'Next',
-		  type: 'next'
-		}
-	  ],
-	  highlightClass: 'highlight',
-	  scrollTo: false,
-	  cancelIcon: {
-		enabled: true,
-	  },
-	  title: 'Hello',
-	  text: ['Create and manage clients'],
-	  when: {
-		show: () => {
-		  console.log('show step');
-		},
-		hide: () => {
-		  console.log('hide step');
-		}
-	  }
-	},
-	{
-	  id: 'intro3',
-	  attachTo: {
-		element: '#menu-item-groups',
-		on: 'right-start'
-	  },
-	  beforeShowPromise: function () {
-		return new Promise(function (resolve) {
-		  setTimeout(function () {
-			window.scrollTo(0, 0);
-			resolve();
-		  }, 500);
-		});
-	  },
-	  buttons: [
-		{
-		  classes: 'tour-button-secondary',
-		  text: 'Exit',
-		  type: 'cancel'
+		  target: '#menu-item-status',
+		  content: 'Monitor the status of your Mosquitto broker.',
 		},
 		{
-		  classes: 'tour-button-primary',
-		  text: 'Back',
-		  type: 'back'
+		  target: '#menu-item-topics',
+		  content: 'Analyse all topics with this awesome topic tree.',
 		},
-		{
-		  classes: 'tour-button-primary',
-		  text: 'Next',
-		  type: 'next'
-		}
-	  ],
-	  highlightClass: 'highlight',
-	  scrollTo: false,
-	  cancelIcon: {
-		enabled: true,
-	  },
-	  title: 'Hello',
-	  text: ['Create and manage groups'],
-	  when: {
-		show: () => {
-		  console.log('show step');
-		},
-		hide: () => {
-		  console.log('hide step');
-		}
-	  }
-	},
-	{
-	  id: 'intro3',
-	  attachTo: {
-		element: '#menu-item-roles',
-		on: 'right-start'
-	  },
-	  beforeShowPromise: function () {
-		return new Promise(function (resolve) {
-		  setTimeout(function () {
-			window.scrollTo(0, 0);
-			resolve();
-		  }, 500);
-		});
-	  },
-	  buttons: [
-		{
-		  classes: 'tour-button-secondary',
-		  text: 'Exit',
-		  type: 'cancel'
-		},
-		{
-		  classes: 'tour-button-primary',
-		  text: 'Back',
-		  type: 'back'
-		},
-		{
-		  classes: 'tour-button-primary',
-		  text: 'Next',
-		  type: 'next'
-		}
-	  ],
-	  highlightClass: 'highlight',
-	  scrollTo: false,
-	  cancelIcon: {
-		enabled: true,
-	  },
-	  title: 'Hello',
-	  text: ['Create and manage roles and ACLs and add them to clients and groups'],
-	  when: {
-		show: () => {
-		  console.log('show step');
-		},
-		hide: () => {
-		  console.log('hide step');
-		}
-	  }
-	},
-  ];
+	];
