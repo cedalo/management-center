293791f44993c86bfb968940d9a08413d6f14dc3
Philip Ackermann <info@philipackermann.de>
2020-11-03 08:05:07 +0100

    Update imports

--- backend/start.js
+++ backend/start.js
@@ -1,4 +1,7 @@
+const fs = require("fs");
+const path = require("path");
 const http = require("http");
+const { v4: uuidv4 } = require('uuid');
 const express = require("express");
 const cors = require('cors');
 const WebSocket = require("ws");
