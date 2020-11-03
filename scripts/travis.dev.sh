e46120044ee058c915cf4d2381cdcfa3158ea71f
Philip Ackermann <info@philipackermann.de>
2020-11-03 08:21:48 +0100

    Create dev Docker image on Travis for master branch

--- scripts/travis.dev.sh
+++ scripts/travis.dev.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker build --build-arg MOSQUITTO_UI_BUILD_DATE="$(date)" --build-arg MOSQUITTO_UI_BUILD_NUMBER="$(date '+%s')" -t cedalo/mosquitto-ui:travis .
-docker push cedalo/mosquitto-ui:travis
+docker push cedalo/mosquitto-ui:dev
