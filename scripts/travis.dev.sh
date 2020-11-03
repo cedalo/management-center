e3968eae46fc36f5fbf361e28f60732ebf037b93
Philip Ackermann <info@philipackermann.de>
2020-11-03 13:36:44 +0100

    Fix Travis build script

--- scripts/travis.dev.sh
+++ scripts/travis.dev.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker build --build-arg MOSQUITTO_UI_BUILD_DATE="$(date)" --build-arg MOSQUITTO_UI_BUILD_NUMBER="$(date '+%s')" -t cedalo/mosquitto-ui:travis .
+docker build --build-arg MOSQUITTO_UI_BUILD_DATE="$(date)" --build-arg MOSQUITTO_UI_BUILD_NUMBER="$(date '+%s')" -t cedalo/mosquitto-ui:dev .
 docker push cedalo/mosquitto-ui:dev
