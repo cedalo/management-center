6abe4082b10400f75d03d223f4e87f7684a6c2ff
Philip Ackermann <info@philipackermann.de>
2020-11-10 18:43:47 +0100

    Add switch component to disable and enable plugins

--- frontend/src/components/Plugins.js
+++ frontend/src/components/Plugins.js
@@ -1,10 +1,13 @@
 import moment from 'moment';
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
+import { connect, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+import { useConfirm } from 'material-ui-confirm';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import PluginDisabledIcon from '@material-ui/icons/Cancel';
 import PluginEnabledIcon from '@material-ui/icons/CheckCircle';
+import Switch from '@material-ui/core/Switch';
+import Tooltip from '@material-ui/core/Tooltip';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
@@ -17,6 +20,7 @@ import { red, green } from '@material-ui/core/colors';
 
 import { Link as RouterLink } from 'react-router-dom';
 
+import { WebSocketContext } from '../websockets/WebSocket';
 import useFetch from '../helpers/useFetch';
 
 const useStyles = makeStyles((theme) => ({
@@ -34,8 +38,43 @@ const useStyles = makeStyles((theme) => ({
 
 const Plugins = (props) => {
 	const classes = useStyles();
+	const context = useContext(WebSocketContext);
+	const dispatch = useDispatch();
+	const confirm = useConfirm();
+	const { client } = context;
 	const [response, loading, hasError] = useFetch(`http://${window.location.hostname}:8088/api/plugins`);
 
+	const handlePluginLoad = async (pluginId, load) => {
+		if (load) {
+			await confirm({
+				title: 'Confirm enabling of plugin',
+				description: `Do you really want to enable the plugin "${pluginId}"?`,
+				cancellationButtonProps: {
+					variant: 'contained'
+				},
+				confirmationButtonProps: {
+					color: 'primary',
+					variant: 'contained'
+				}
+			});
+			await client.loadPlugin(pluginId);
+			window.location.reload();
+		} else {
+			await confirm({
+				title: 'Confirm disabling of plugin',
+				description: `Do you really want to disable the plugin "${pluginId}"?`,
+				cancellationButtonProps: {
+					variant: 'contained'
+				},
+				confirmationButtonProps: {
+					color: 'primary',
+					variant: 'contained'
+				}
+			});
+			await client.unloadPlugin(pluginId);
+			window.location.reload();
+		}
+	};
 	if (response) {
 		return (
 			<div>
@@ -58,6 +97,7 @@ const Plugins = (props) => {
 									<TableCell>Description</TableCell>
 									<TableCell>Feature</TableCell>
 									<TableCell>Status</TableCell>
+									<TableCell>Actions</TableCell>
 								</TableRow>
 							</TableHead>
 							<TableBody>
@@ -80,10 +120,23 @@ const Plugins = (props) => {
 										</TableCell>
 										<TableCell>
 											{
-												plugin.status === 'loaded' 
+												plugin.status.type === 'loaded' 
 												? <PluginEnabledIcon fontSize="small" style={{ color: green[500] }} /> 
-												: <PluginDisabledIcon fontSize="small" style={{ color: red[500] }} />
+												: <Tooltip title={plugin.status.message ? plugin.status.message : null}>
+													<PluginDisabledIcon fontSize="small" style={{ color: red[500] }} />
+												  </Tooltip>
 											}
+										</TableCell>
+										<TableCell>
+											<Switch
+												disabled={plugin.status.type === 'error'}
+												checked={plugin.status.type === 'loaded'}
+												name="pluginLoaded"
+												onChange={(event) => {
+													handlePluginLoad(plugin.id, event.target.checked);
+												}}
+												inputProps={{ 'aria-label': 'Plugin loaded' }}
+											/>
 										</TableCell>
 									</TableRow>
 								))}
