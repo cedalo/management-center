ddcec574e47038d713926d03da69b5eca163a068
Philip Ackermann <info@philipackermann.de>
2020-11-10 18:40:14 +0100

    Update plugin structure for Topic Tree REST API

--- backend/plugins/topictree-rest-api/src/Plugin.js
+++ backend/plugins/topictree-rest-api/src/Plugin.js
@@ -1,45 +1,45 @@
 const BasePlugin = require('../../base/src/BasePlugin');
+const meta = require('./meta');
 
 module.exports = class Plugin extends BasePlugin {
 	constructor() {
 		super();
-		this._meta = {
-			"id": "cedalo_topic_tree_rest_api",
-			"name": "Cedalo Topic Tree REST API",
-			"version":"1.0",
-			"description":"Access the topic tree via REST API",
-			"feature": "REST API"
-		}
+		this._meta = meta;
 	}
 
 	init(context) {
 		const { actions, app, licenseContainer, globalTopicTree, brokerManager } = context;
 		app.get('/api/system/topictree', (request, response) => {
-			if (licenseContainer.license.isValid) {
-				response.json(globalTopicTree);
+			if (this.isLoaded()) {
+				if (licenseContainer.license.isValid) {
+					response.json(globalTopicTree);
+				} else {
+					response.status(404).send('Not supported with the given license.');
+				}
 			} else {
-				response.status(404).send('Not supported with the given license.');
+				response.status(404).send('Plugin not loaded');
 			}
 		});
 	
 		app.delete('/api/system/topictree', (request, response) => {
-			Object.keys(globalTopicTree).forEach((brokerName) => {
-				const topicTree = globalTopicTree[brokerName];
-				Object.keys(topicTree).forEach((key) => {
-					delete topicTree[key];
+			if (this.isLoaded()) {
+				Object.keys(globalTopicTree).forEach((brokerName) => {
+					const topicTree = globalTopicTree[brokerName];
+					Object.keys(topicTree).forEach((key) => {
+						delete topicTree[key];
+					});
+					const brokerConnection = brokerManager.getBrokerConnection(brokerName);
+					if (brokerConnection) {
+						const { broker, system, topicTree } = brokerConnection;
+						actions.sendSystemStatusUpdate(system, broker);
+						actions.sendTopicTreeUpdate(topicTree, broker);
+					}
 				});
-				const brokerConnection = brokerManager.getBrokerConnection(brokerName);
-				if (brokerConnection) {
-					const { broker, system, topicTree } = brokerConnection;
-					actions.sendSystemStatusUpdate(system, broker);
-					actions.sendTopicTreeUpdate(topicTree, broker);
-				}
-			});
-			response.send();
+				response.send();
+			} else {
+				response.status(404).send();
+			}
 		});
-	}
-
-	load(context) {
 	}
 
 	get meta() {
