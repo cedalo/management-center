0df983dbbeab9cb387e94d3cac4308bf442a18a8
Philip Ackermann <info@philipackermann.de>
2020-11-13 19:23:40 +0100

    Show graphic when broker connection was disconnected

--- frontend/src/components/DisconnectedDialog.js
+++ frontend/src/components/DisconnectedDialog.js
@@ -6,8 +6,12 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 import BrokerSelect from './BrokerSelect';
+// import MessagePage from './MessagePage';
 
 const DisconnectedDialog = ({ connected }) => {
 	const handleClose = () => {
@@ -21,10 +25,26 @@ const DisconnectedDialog = ({ connected }) => {
 			aria-labelledby="not-connected-dialog-title"
 			aria-describedby="not-connected-dialog-description"
 		>
-			<DialogTitle id="not-connected-dialog-title">Connection lost</DialogTitle>
+			<DialogTitle align="center" id="not-connected-dialog-title">We could not connect to your broker</DialogTitle>
 			<DialogContent>
-				<DialogContentText id="alert-dialog-description">Please select another connection</DialogContentText>
-				<BrokerSelect />
+				{/* <MessagePage 
+					message="We could not find any Streamsheets installation."
+					buttonText="Get Streamsheets now!"
+				/> */}
+				<Grid
+					container
+					spacing={24}
+					justify="center"
+					style={{ maxWidth: '100%' }}
+				>
+					<Grid item xs={12} align="center">
+						<img src="/disconnected.png" />
+					</Grid>
+					<Grid item xs={12} align="center">
+						<DialogContentText id="alert-dialog-description">Please select another connection</DialogContentText>
+						<BrokerSelect />
+					</Grid>
+				</Grid>
 			</DialogContent>
 			<DialogActions>
 			</DialogActions>
