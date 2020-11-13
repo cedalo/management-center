bc90bfe4d87556ceefec18ba1d7227b210a2260f
Philip Ackermann <info@philipackermann.de>
2020-11-13 19:23:59 +0100

    Add message page component for displaying a status message and a status graphic

---
+++ frontend/src/components/MessagePage.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import DownloadIcon from '@material-ui/icons/GetApp';
+import { amber } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+	button: {
+		margin: theme.spacing(1),
+	},
+	updateButton: {
+		marginLeft: '20px'
+	},
+	badges: {
+		'& > *': {
+			margin: theme.spacing(0.3)
+		}
+	},
+	breadcrumbItem: theme.palette.breadcrumbItem,
+	breadcrumbLink: theme.palette.breadcrumbLink
+}));
+
+const MessagePage = ({ message, buttonText, buttonIcon, callToAction }) => {
+	const classes = useStyles();
+	return (<Paper>
+		<Grid
+			container
+			spacing={24}
+			justify="center"
+			style={{ minHeight: '500px', maxWidth: '100%' }}
+		>
+		<Grid item xs={12} align="center">
+			
+		</Grid>
+	   <Grid item xs={12} align="center">
+			   <img src="/smilethink.png" />
+			   <Typography variant="h6" gutterBottom>
+				{ message }
+			</Typography>
+		</Grid>
+	   <Grid item xs={12} align="center">
+			<Button
+				variant="contained"
+				style={{ backgroundColor: amber[500] }} 
+				className={classes.button}
+				startIcon={ buttonIcon || <DownloadIcon />}
+				onClick={(event) => {
+					event.stopPropagation();
+					callToAction();
+				}}
+				size="small"
+			>
+				{ buttonText }
+			</Button>
+		</Grid>
+	  </Grid>
+	</Paper>)
+};
+
+const mapStateToProps = (state) => {
+	return {
+	};
+};
+
+export default connect(mapStateToProps)(MessagePage);
