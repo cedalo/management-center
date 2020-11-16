ed97a00b205c0fc7cf7875aa71690091e25e54c8
Philip Ackermann <info@philipackermann.de>
2020-11-16 16:59:25 +0100

    Add coming soon page for Streams

--- frontend/src/components/Streams.js
+++ frontend/src/components/Streams.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { useConfirm } from 'material-ui-confirm';
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import Typography from '@material-ui/core/Typography';
 
-function Streams() {
-	return 'Streams';
-}
+import { Link as RouterLink } from 'react-router-dom';
 
-export default Streams;
+import MessagePage from './MessagePage';
+
+const useStyles = makeStyles((theme) => ({
+	breadcrumbItem: theme.palette.breadcrumbItem,
+	breadcrumbLink: theme.palette.breadcrumbLink
+}));
+
+const Streams = (props) => {
+	const classes = useStyles();
+	return (
+		<div>
+			<Breadcrumbs aria-label="breadcrumb">
+				<RouterLink className={classes.breadcrumbLink} to="/home">
+					Home
+				</RouterLink>
+				<Typography className={classes.breadcrumbItem} color="textPrimary">
+					Streams
+				</Typography>
+			</Breadcrumbs>
+			<br />
+			<MessagePage 
+				message="Pssst!!! We are working on a secret feature. Coming soon."
+				image="/inprogress.png"
+			/>
+		</div>
+	)
+};
+
+const mapStateToProps = (state) => {
+	return {
+	};
+};
+
+export default connect(mapStateToProps)(Streams);
