30c9f46839f0e06344ec97ecdc1d0b124c3af246
Philip Ackermann <info@philipackermann.de>
2020-11-20 16:04:00 +0100

    Add anonymous group to groups reducer

--- frontend/src/reducers/groupsReducer.js
+++ frontend/src/reducers/groupsReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../constants/ActionTypes';
 export default function groups(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_ANONYMOUS_GROUP:
+			newState.anonymousGroup = action.update;
+			break;
 		case ActionTypes.UPDATE_GROUP:
 			newState.group = action.update;
 			break;
