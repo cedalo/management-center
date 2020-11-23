da077e63c4c4114bb14b52ce58d2145ad5b6fde4
Philip Ackermann <info@philipackermann.de>
2020-11-23 09:19:15 +0100

    Update version number

--- backend/start.js
+++ backend/start.js
@@ -14,6 +14,7 @@ const PluginManager = require('./src/plugins/PluginManager');
 
 const version = {
 	name: process.env.CEDALO_MC_NAME || 'Cedalo Management Center',
+	version: process.env.CEDALO_MC_VERSION || '2.0',
 	buildNumber: process.env.TRAVIS_BUILD_NUMBER || process.env.CEDALO_MC_BUILD_NUMBER || uuidv4(),
 	buildDate: process.env.CEDALO_MC_BUILD_DATE || Date.now()
 };
