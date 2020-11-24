98489ba5e6cfcc95a5c1246157e232921816590c
Philip Ackermann <info@philipackermann.de>
2020-11-24 11:25:12 +0100

    Add default ACL access to role reducer

--- frontend/src/reducers/rolesReducer.js
+++ frontend/src/reducers/rolesReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../constants/ActionTypes';
 export default function roles(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_DEFAULT_ACL_ACESS:
+			newState.defaultACLAccess = action.update;
+			break;
 		case ActionTypes.UPDATE_ROLE:
 			newState.role = action.update;
 			break;
