9cb9d9f97d79310e8953d944bf57bc7bb9bd6e41
Philip Ackermann <info@philipackermann.de>
2021-01-13 14:35:14 +0100

    Use lowdb for saving installation information

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+
+const adapter = new FileSync('db.json');
+const db = low(adapter);
 
 const initId = () => {
-	const id = uuidv4();
+	const id = 	uuidv4();
 	return {
 		id,
 		created: Date.now()
@@ -11,19 +16,10 @@ const initId = () => {
 }
 
 const loadInstallation = () => {
-	const installationFile = '../../../config/installation.json';
-	let installation;
-	if (fs.existsSync(path.resolve(__dirname, installationFile))) {
-		try {
-			installation = JSON.parse(fs.readFileSync(path.resolve(__dirname, installationFile)).toString());
-		} catch (error) {
-			installation = initId();
-			fs.writeFileSync(path.resolve(__dirname, installationFile), JSON.stringify(installation, null, 2));
-		}
-	} else {
-		installation = initId();
-		fs.writeFileSync(path.resolve(__dirname, installationFile), JSON.stringify(installation, null, 2));
-	}
+	db.defaults({
+		install: initId()
+	}).write();
+	const installation = db.get('install').value();
 	return installation;
 }
 
