28902d5c384e27136c56299f75dda73eca1e1dca
Philip Ackermann <info@philipackermann.de>
2021-03-18 10:12:04 +0100

    Add logout button component

---
+++ frontend/src/components/LogoutButton.js
@@ -0,0 +1,56 @@
+import React, { useContext, useState } from 'react';
+import { connect, useDispatch } from 'react-redux';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import LogoutIcon from '@material-ui/icons/ExitToApp';
+import { makeStyles } from '@material-ui/core/styles';
+import { useConfirm } from 'material-ui-confirm';
+
+
+const useStyles = makeStyles((theme) => ({
+	toolbarButton: {
+		marginTop: theme.spacing(0.8),
+		marginBottom: theme.spacing(0.2)
+	},
+}));
+
+const LogoutButton = (props) => {
+	const classes = useStyles();
+	const confirm = useConfirm();
+
+	const handleLogout = async () => {
+		await confirm({
+			title: 'Confirm logout',
+			description: `Do you really want to logout?`,
+			cancellationButtonProps: {
+				variant: 'contained'
+			},
+			confirmationButtonProps: {
+				color: 'primary',
+				variant: 'contained'
+			}
+		});
+		window.location.href = '/logout';
+	};
+
+	return <Tooltip title="Logout">
+		<IconButton
+			edge="end"
+			aria-label="Logout"
+			aria-controls="logout"
+			aria-haspopup="true"
+			onClick={() => handleLogout()}
+			color="inherit"
+			className={classes.toolbarButton}
+		>
+			<LogoutIcon />
+		</IconButton>
+	</Tooltip>
+};
+
+const mapStateToProps = (state) => {
+	return {
+	};
+};
+
+export default connect(mapStateToProps)(LogoutButton);
