c6aecd0fe9fd69edf4212b291be8596a50f9bb8a
Philip Ackermann <info@philipackermann.de>
2021-04-16 16:20:08 +0200

    Add index file for license classes

---
+++ backend/src/license/index.js
@@ -0,0 +1,9 @@
+const License = require('./License');
+const LicenseChecker = require('./LicenseChecker');
+const LicenseKey = require('./LicenseKey');
+
+module.exports = {
+    License,
+    LicenseChecker,
+    LicenseKey
+}
