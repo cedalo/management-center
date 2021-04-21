30b06b61859db17bb5aaba9b83a98a6920c65ba1
Philip Ackermann <info@philipackermann.de>
2021-04-21 14:45:35 +0200

    Fix topic tree flickering

--- frontend/src/reducers/topicTreeReducer.js
+++ frontend/src/reducers/topicTreeReducer.js
@@ -1,10 +1,19 @@
 import * as ActionTypes from '../constants/ActionTypes';
 
+// TODO: quick fix, remove if this is fixed on server-side
+let currentTopicTreeConnectionName;
+
 export default function topicTreeReducer(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
 		case ActionTypes.UPDATE_TOPIC_TREE:
-			newState.topicTree = action.update;
+			// Quick fix: only update if selected topic tree is the same
+			if (state.topicTree === undefined || currentTopicTreeConnectionName === action.update._name) {
+				newState.topicTree = action.update;
+			}
+			break;
+		case ActionTypes.UPDATE_BROKER_CONNECTED:
+			currentTopicTreeConnectionName = action.update.connectionName;
 			break;
 		default:
 	}
