7c807b04a6ee7c6b5cb2abb852e295db92f82c46
Philip Ackermann <info@philipackermann.de>
2021-04-22 10:25:36 +0200

    Fix system status conflicts

--- frontend/src/reducers/systemStatusReducer.js
+++ frontend/src/reducers/systemStatusReducer.js
@@ -1,11 +1,20 @@
 import * as ActionTypes from '../constants/ActionTypes';
 
+// TODO: quick fix, remove if this is fixed on server-side
+let currentTopicTreeConnectionName;
+
 export default function systemStatus(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
 		case ActionTypes.UPDATE_SYSTEM_STATUS:
-			newState.systemStatus = action.update;
-			newState.lastUpdated = Date.now();
+			// Quick fix: only update if selected topic tree is the same
+			if (state.systemStatus === undefined || currentTopicTreeConnectionName === action.update._name) {
+				newState.systemStatus = action.update;
+				newState.lastUpdated = Date.now();
+			}
+			break;
+		case ActionTypes.UPDATE_BROKER_CONNECTED:
+			currentTopicTreeConnectionName = action.update.connectionName;
 			break;
 		default:
 	}
