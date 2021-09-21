b5e4a04a39c35f2c9eb94f4a680a789fe81cbf9f
Philip Ackermann <info@philipackermann.de>
2021-09-21 10:39:59 +0200

    Print message if no license key was found fixes #17

--- backend/src/license/utils/loadLicense.js
+++ backend/src/license/utils/loadLicense.js
@@ -7,9 +7,14 @@ const readFile = require('./readFile');
 const loadPublicKey = () =>
 	readFile(process.env.CEDALO_MC_LICENSE_PUBLIC_KEY_PATH || path.join(__dirname, 'config', 'public.pem'));
 const loadLicenseKey = () =>
-	readFile(process.env.CEDALO_MC_LICENSE_PATH || path.join(__dirname, 'config', 'license.lic')).then((key) =>
+	readFile(process.env.CEDALO_MC_LICENSE_PATH || path.join(__dirname, 'config', 'license.lic'))
+	.then((key) =>
 		key.toString()
-	);
+	)
+	.catch((error) => {
+		console.log('No license key found or provided.');
+		throw error;
+	})
 
 const loadLicense = async () => {
 	let license;
@@ -18,9 +23,6 @@ const loadLicense = async () => {
 		const licenseKey = await loadLicenseKey();
 		license = LicenseKey.verify(licenseKey, publicKey);
 	} catch (error) {
-		console.error(error);
-		console.error(__dirname);
-		console.error(process.env.CEDALO_MC_LICENSE_PATH);
 		license = License.Invalid;
 	}
 	return license;
