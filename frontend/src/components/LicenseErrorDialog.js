d2c25e8fb796c3613a614719c3c72af897ed828d
Philip Ackermann <info@philipackermann.de>
2021-10-23 17:56:53 +0200

    Improve license error dialog

--- frontend/src/components/LicenseErrorDialog.js
+++ frontend/src/components/LicenseErrorDialog.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 import { connect } from 'react-redux';
 
 const LicenseErrorDialog = ({ license }) => {
@@ -15,7 +19,14 @@ const LicenseErrorDialog = ({ license }) => {
 			aria-labelledby="license-error-dialog-title"
 			aria-describedby="license-error-dialog-description"
 		>
-			License error
+			<DialogTitle align="center" id="license-error-dialog-title">
+				{ license?.error?.type }
+			</DialogTitle>
+			<DialogContent>
+				<DialogContentText id="license-error--dialog-description">
+				{ license?.error?.message }
+				</DialogContentText>
+			</DialogContent>
 		</Dialog>
 	);
 };
