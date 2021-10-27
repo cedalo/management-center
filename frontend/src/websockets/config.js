7456d6ab344b0e542f49d7866e75abe3b6074c18
Philip Ackermann <info@philipackermann.de>
2021-10-27 20:20:02 +0200

    Include process.env.PUBLIC_URL in WebSockets configuration

--- frontend/src/websockets/config.js
+++ frontend/src/websockets/config.js
@@ -2,5 +2,5 @@ const { protocol } = window.location;
 const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
 
 export default {
-	url: `${wsProtocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`
+	url: `${wsProtocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`
 };
