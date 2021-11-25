34a5b94a4552c7e51cb8a57098ce121263705c84
Philip Ackermann <info@philipackermann.de>
2021-11-25 18:48:54 +0100

    Update userProfileReducer.js

--- frontend/src/reducers/userProfileReducer.js
+++ frontend/src/reducers/userProfileReducer.js
@@ -5,6 +5,9 @@ export default function userProfileReducer(state = {}, action) {
 	switch (action.type) {
 		case ActionTypes.UPDATE_USER_PROFILE:
 			newState.userProfile = action.update;
+			newState.userProfile.isAdmin = newState.userProfile.roles.includes('admin');
+			newState.userProfile.isEditor = newState.userProfile.roles.includes('editor');
+			newState.userProfile.isViewer = newState.userProfile.roles.includes('viewer');
 			break;
 		default:
 	}
