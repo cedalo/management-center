213226c86fe02fde334941bfb0f001dee6fc75d4
Philip Ackermann <info@philipackermann.de>
2021-12-01 19:01:22 +0100

    Update cluster reducer

--- frontend/src/admin/clusters/reducers/clustersReducer.js
+++ frontend/src/admin/clusters/reducers/clustersReducer.js
@@ -3,6 +3,9 @@ import * as ActionTypes from '../actions/ActionTypes';
 export default function clusters(state = {}, action) {
 	const newState = { ...state };
 	switch (action.type) {
+		case ActionTypes.UPDATE_CLUSTER:
+			newState.cluster = action.update;
+			break;
 		case ActionTypes.UPDATE_CLUSTERS:
 			newState.clusters = action.update;
 			break;
