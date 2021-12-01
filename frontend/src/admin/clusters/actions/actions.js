d12c7a757a74e5e9955bdb987b68f4e214cb14fe
Philip Ackermann <info@philipackermann.de>
2021-12-01 19:00:36 +0100

    Add action to update a cluster

--- frontend/src/admin/clusters/actions/actions.js
+++ frontend/src/admin/clusters/actions/actions.js
@@ -1,5 +1,12 @@
 import * as ActionTypes from './ActionTypes';
 
+export function updateCluster(update) {
+	return {
+		type: ActionTypes.UPDATE_CLUSTER,
+		update
+	};
+}
+
 export function updateClusters(update) {
 	return {
 		type: ActionTypes.UPDATE_CLUSTERS,
