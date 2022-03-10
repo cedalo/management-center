ab3447120c8cce0bf6c58e07ac0177d0a785c71f
Philip Ackermann <info@philipackermann.de>
2022-03-10 12:35:29 +0100

    Update config.js

--- frontend/src/websockets/config.js
+++ frontend/src/websockets/config.js
@@ -3,6 +3,6 @@ const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
 
 export default {
 	// url: `${wsProtocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`
-	url: `${wsProtocol}//${window.location.hostname}:8088${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
-	urlHTTP: `${protocol}//${window.location.hostname}:8088${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
+	url: `${wsProtocol}//${window.location.host}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
+	urlHTTP: `${protocol}//${window.location.host}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
 };
