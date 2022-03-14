45e7503652039f15c7979ae727509930ea4621ba
Philip Ackermann <info@philipackermann.de>
2022-03-14 12:05:15 +0100

    Fix WebSocket path

--- frontend/src/websockets/config.js
+++ frontend/src/websockets/config.js
@@ -1,8 +1,9 @@
 const { protocol } = window.location;
 const wsProtocol = protocol === 'https:' ? 'wss:' : 'ws:';
+const path = `${(process.env.PUBLIC_URL && process.env.PUBLIC_URL !== '.') ? process.env.PUBLIC_URL : ''}`;
 
 export default {
 	// url: `${wsProtocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`
-	url: `${wsProtocol}//${window.location.host}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
-	urlHTTP: `${protocol}//${window.location.host}${process.env.PUBLIC_URL ? process.env.PUBLIC_URL : ''}`,
+	url: `${wsProtocol}//${window.location.host}${path}`,
+	urlHTTP: `${protocol}//${window.location.host}${path}`,
 };
