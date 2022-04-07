8b469fb926e1ef6dc9791fb73d9a2995172054e0
Philip Ackermann <info@philipackermann.de>
2022-04-07 15:22:08 +0200

    Revert "Add commit id to info page"
    This reverts commit 2261f28a4e1771827e3ce7d46fca09caf723027d.

--- backend/start.js
+++ backend/start.js
@@ -24,7 +24,6 @@ const version = {
 	name: process.env.CEDALO_MC_NAME || 'Cedalo Management Center',
 	version: process.env.CEDALO_MC_VERSION || packageJSON.version,
 	buildNumber: process.env.TRAVIS_BUILD_NUMBER || process.env.CEDALO_MC_BUILD_NUMBER || uuidv4(),
-	commitId: process.env.TRAVIS_COMMIT || process.env.CEDALO_COMMIT || 'unknown',
 	buildDate: process.env.CEDALO_MC_BUILD_DATE || Date.now()
 };
 
