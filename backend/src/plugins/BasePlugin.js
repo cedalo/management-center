807cb31649974a1c81b29c585c62c6bde3ab7488
Philip Ackermann <info@philipackermann.de>
2022-04-27 15:33:27 +0200

    Add logging to base plugin

--- backend/src/plugins/BasePlugin.js
+++ backend/src/plugins/BasePlugin.js
@@ -1,12 +1,27 @@
+const winston = require('winston');
+
 const STATUS_ERROR = 'error';
 const STATUS_UNLOADED = 'unloaded';
 const STATUS_LOADED = 'loaded';
 
 module.exports = class BasePlugin {
-	constructor() {
+	constructor(meta) {
 		this._status = {
 			type: STATUS_UNLOADED
 		};
+		const logger = winston.createLogger({
+			level: 'info',
+			format: winston.format.json(),
+			defaultMeta: { service: meta?.name },
+			transports: [
+			  new winston.transports.File({ filename: `plugin-${meta?.id}.log` }),
+			],
+		  });
+		this._logger = logger;
+	}
+
+	get logger() {
+		return this._logger;
 	}
 
 	unload(context) {
