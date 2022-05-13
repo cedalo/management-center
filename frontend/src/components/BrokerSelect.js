bb15927d54f126eff748e6b4844953c91700c6a0
Philip Ackermann <info@philipackermann.de>
2022-05-13 13:50:19 +0200

    Add pagination parameters when sending request

--- frontend/src/components/BrokerSelect.js
+++ frontend/src/components/BrokerSelect.js
@@ -98,13 +98,13 @@ const BrokerSelect = ({ brokerConnections, connected, currentConnectionName, sen
 			const brokerConfigurations = await client.getBrokerConfigurations();
 			dispatch(updateBrokerConfigurations(brokerConfigurations));
 			try {
-				const clients = await client.listClients();
+				const clients = await client.listClients(true, 10, 0);
 				dispatch(updateClients(clients));
-				const groups = await client.listGroups();
+				const groups = await client.listGroups(true, 10, 0);
 				dispatch(updateGroups(groups));
 				const anonymousGroup = await client.getAnonymousGroup();
 				dispatch(updateAnonymousGroup(anonymousGroup));
-				const roles = await client.listRoles();
+				const roles = await client.listRoles(true, 10, 0);
 				dispatch(updateRoles(roles));
 				const defaultACLAccess = await client.getDefaultACLAccess();
 				dispatch(updateDefaultACLAccess(defaultACLAccess));
