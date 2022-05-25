7c4530ec2b889f753b6da8d37618b25b138123eb
Philip Ackermann <info@philipackermann.de>
2022-05-25 10:09:05 +0200

    Add Swagger documentation for user-profile endpoint

---
+++ backend/src/plugins/user-profile/src/swagger.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "tags": [
+        {
+            "name": "user-profile",
+            "description": "User Profile API"
+        }
+    ],
+    "paths": {
+        "/api/profile": {
+            "get": {
+                "tags": [
+                    "user-profile"
+                ],
+                "summary": "Returns the profile of the currently logged in user.",
+                "produces": [
+                    "application/json"
+                ],
+                "responses": {
+                    "200": {
+                        "description": "OK"
+                    }
+                }
+            }
+        }
+    }
+}
