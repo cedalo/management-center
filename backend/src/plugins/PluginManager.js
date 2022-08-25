51ed1ac2e36b38a63e7e53a27036566d618aaff7
Sergey <serhii.orlivskyi@gmail.com>
2022-08-25 18:40:00 +0200

    Add components to swagger

--- backend/src/plugins/PluginManager.js
+++ backend/src/plugins/PluginManager.js
@@ -105,6 +105,7 @@ module.exports = class PluginManager {
 				if (plugin.swagger) {
 					swaggerDocument.tags = Object.assign(swaggerDocument.tags || {}, plugin.swagger.tags);
 					swaggerDocument.paths = Object.assign(swaggerDocument.paths || {}, plugin.swagger.paths);
+					swaggerDocument.components = Object.assign(swaggerDocument.components || {}, plugin.swagger.components);
 				}
 
 				if (plugin._status.type !== 'error') {
--- backend/swagger.js
+++ backend/swagger.js
@@ -15,5 +15,7 @@ module.exports = {
     "https"
   ],
   "paths": {
+  },
+  "components": {
   }
 }
