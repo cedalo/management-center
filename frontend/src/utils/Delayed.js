3d1e14d2c144a2e45e08b0de3366b327a4f9aac5
Sergey <serhii.orlivskyi@gmail.com>
2022-11-18 10:27:29 +0100

    Add delayed render component

---
+++ frontend/src/utils/Delayed.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+
+const Delayed = ({ children, waitBeforeShow = 500 }) => {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShown(true);
+    }, waitBeforeShow);
+    return () => clearTimeout(timer);
+  }, [waitBeforeShow]);
+
+  return isShown ? children : null;
+};
+
+export default Delayed;
