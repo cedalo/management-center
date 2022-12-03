93d38a126831ec83f5538493ad359f710e14253b
Sergey <serhii.orlivskyi@gmail.com>
2022-12-03 01:03:17 +0100

    Add version console log

--- backend/start.js
+++ backend/start.js
@@ -38,6 +38,8 @@ const CEDALO_MC_ENABLE_FULL_LOG = !!process.env.CEDALO_MC_ENABLE_FULL_LOG;
 
 const CEDALO_MC_PROXY_BASE_PATH = process.env.CEDALO_MC_PROXY_BASE_PATH || '';
 const USAGE_TRACKER_INTERVAL = 1000 * 60 * 60;
+
+console.log(`Mosquitto Management Center version ${version.version || 'unknown'}`)
 console.log(`MMC is starting in the ${process.env.CEDALO_MC_MODE === 'offline' ? 'offline' : 'online'} mode`);
 
 // const LicenseManager = require("../src/LicenseManager");
