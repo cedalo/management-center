6b281584f8f1a92060f67ddf606d0037ff69d633
Sergey <serhii.orlivskyi@gmail.com>
2022-12-27 13:13:36 +0100

    Fix: sub to all topics only when topic tree enabled

--- backend/start.js
+++ backend/start.js
@@ -378,12 +378,6 @@ const init = async (licenseContainer) => {
 					console.error(error);
 				}
 			});
-			brokerClient.subscribe('#', (error) => {
-				console.log(`Subscribed to all topics for '${connection.name}'`);
-				if (error) {
-					console.error(error);
-				}
-			});
 			brokerClient.subscribe('$CONTROL/dynamic-security/v1/#', (error) => {
 				console.log(`Subscribed to all topics for '${connection.name}'`);
 				if (error) {
