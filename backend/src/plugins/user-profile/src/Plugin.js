5869dea23c8863234c82e1753922efc97eb3cb6e
Andreas Sayn <andreas.sayn@cedalo.com>
2023-02-02 12:19:51 +0100

    Implement actions for user-profile plugin

--- backend/src/plugins/user-profile/src/Plugin.js
+++ backend/src/plugins/user-profile/src/Plugin.js
@@ -13,23 +13,23 @@ module.exports = class Plugin extends BasePlugin {
 	init(context) {
 		const { router } = context;
 
-		router.get('/api/profile',
+		const { getProfileAction } = createActions(this);
+
+		context.registerAction(getProfileAction);
+
+		router.get(
+			'/api/profile',
 			// we need this wrapper becuse in some plugins like application-tokens we are going to redefine context.security.isLoggedIn, so we need it to be resolved dynamically via a wrapper
 			(request, response, next) => context.security.isLoggedIn(request, response, next),
+			context.middleware.isPluginLoaded(this),
 			(request, response) => {
-				if (this.isLoaded()) {
-					const result = request.user;
-					delete result.password;
-					response.json(result);
-				} else {
-					this.sendResponsePluginNotEnabled(response);
-				}
+				const result = context.runAction(request.user, 'user-profile/get');
+				response.send(result);
 			}
 		);
-
 	}
 
 	get meta() {
 		return this._meta;
 	}
-}
+};
