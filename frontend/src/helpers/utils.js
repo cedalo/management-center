fddce01f85b30551a1902d128200b5cdca11e1d3
tenhouben <arnd.houben@cedalo.com>
2023-04-04 15:48:59 +0200

    frontend: add util function to get roles used by admin from current state

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -1,7 +1,15 @@
+const getBrokerById = (brokerConnections, id) =>
+	brokerConnections.find((brokerConnection) => brokerConnection.id === id);
 
+const getAdminRolesFromState = (state) => {
+	const admin = state.brokerConnections?.defaultClient;
+	const clients = state.clients?.clients?.clients;
+	if (admin && clients) {
+		const adminName = admin.username;
+		const adminClient = clients.find((c) => c.username === adminName);
+		if (adminClient) return adminClient.roles.map((r) => r.rolename);
+	}
+	return [];
+};
 
-const getBrokerById = (brokerConnections, id) => brokerConnections.find(brokerConnection => brokerConnection.id === id);
-
-export {
-    getBrokerById
-}
+export { getAdminRolesFromState, getBrokerById };
