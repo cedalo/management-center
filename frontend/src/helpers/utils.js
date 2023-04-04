a19c44683f2a932dabc26346786cf45c2ddbd83a
tenhouben <arnd.houben@cedalo.com>
2023-04-04 17:46:39 +0200

    frontend: add util function to check if client is admin

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -1,15 +1,17 @@
 const getBrokerById = (brokerConnections, id) =>
 	brokerConnections.find((brokerConnection) => brokerConnection.id === id);
 
-const getAdminRolesFromState = (state) => {
-	const admin = state.brokerConnections?.defaultClient;
-	const clients = state.clients?.clients?.clients;
-	if (admin && clients) {
-		const adminName = admin.username;
-		const adminClient = clients.find((c) => c.username === adminName);
-		if (adminClient) return adminClient.roles.map((r) => r.rolename);
-	}
-	return [];
+const isAdminClient = (state) => {
+    const defClientUsername = state.brokerConnections?.defaultClient?.username;
+    // default client or its username not always defined
+    return (client) => (defClientUsername ? defClientUsername === client.username : client.username === 'admin');
 };
 
-export { getAdminRolesFromState, getBrokerById };
+const getAdminRolesFromState = (state) => {
+    const isAdmin = isAdminClient(state);
+	const clients = state.clients?.clients?.clients || [];
+	const adminClient = clients.find(isAdmin);
+	return adminClient ? adminClient.roles.map((r) => r.rolename) : [];
+};
+
+export { isAdminClient, getAdminRolesFromState, getBrokerById };
