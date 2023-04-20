4abbdf5ab29fd77150780830c4c60819adbbea61
cabane-mr <michael.raue@cedalo.com>
2023-04-20 12:15:54 +0200

    layout feedback button

--- frontend/src/components/FeedbackButton.js
+++ frontend/src/components/FeedbackButton.js
@@ -1,55 +1,51 @@
 import Button from '@material-ui/core/Button';
-import { indigo } from '@material-ui/core/colors';
+import {indigo} from '@material-ui/core/colors';
 import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import FeedbackIcon from '@material-ui/icons/Feedback';
-import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {connect} from 'react-redux';
 
 
 const ColorButton = withStyles((theme) => ({
 	root: {
-		color: 'black',
-		backgroundColor: 'white',
+		marginTop: '2px',
+		// color: 'black',
+		// backgroundColor: 'white',
 		'&:hover': {
-			backgroundColor: indigo[100],
+			backgroundColor: 'rgba(0, 0, 0, 0.04)',
 		},
 	}
 }))(IconButton);
 
-const FeedbackButton = ({ backendParameters }) => {
-    const [displayFeedback, setDisplayFeedback] = useState(backendParameters.showFeedbackForm);
+const FeedbackButton = ({backendParameters}) => {
+	const [displayFeedback, setDisplayFeedback] = useState(backendParameters.showFeedbackForm);
 	const small = useMediaQuery(theme => theme.breakpoints.down('xs'));
 
-    useEffect(() => {
-        setDisplayFeedback(backendParameters?.showFeedbackForm);
-    }, [backendParameters]);
+	useEffect(() => {
+		setDisplayFeedback(backendParameters?.showFeedbackForm);
+	}, [backendParameters]);
 
-    const formPageAddress = 'https://majy33976q6.typeform.com/to/aeRoINk0';
+	const formPageAddress = 'https://majy33976q6.typeform.com/to/aeRoINk0';
 
-
-	return (true) ? (
-	// return (!small && displayFeedback) ? (
-		<>
-            <ColorButton
-                    variant="contained"
-                    size="small"
-                    target="_blank"
-                    href={formPageAddress}
-            >
-				<FeedbackIcon  />
-            </ColorButton>
-		</>)
-        :
-        <></>;
+	return (!small && displayFeedback) ? (
+			<ColorButton
+				variant="contained"
+				color="primary"
+				target="_blank"
+				href={formPageAddress}
+			>
+				<FeedbackIcon/>
+			</ColorButton>
+		) : null;
 };
 
 const mapStateToProps = (state) => {
-    return {
-      backendParameters: state.backendParameters?.backendParameters,
-      version: state.version?.version
-    };
-  };
+	return {
+		backendParameters: state.backendParameters?.backendParameters,
+		version: state.version?.version
+	};
+};
 
 export default connect(mapStateToProps)(FeedbackButton);
