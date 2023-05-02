0f98f567e9851369c431210c131e2009dcd790a0
cabane-mr <michael.raue@cedalo.com>
2023-05-02 11:46:13 +0200

    update help to urls to release

--- frontend/src/components/HelpButtons.js
+++ frontend/src/components/HelpButtons.js
@@ -53,7 +53,6 @@ export default function HelpButtons(props) {
 		case 'clientinspection':
 			link += 'inspection/mc-clientinspection';
 			break;
-		case 'test':
 		case 'clients':
 		case 'groups':
 		case 'roles':
