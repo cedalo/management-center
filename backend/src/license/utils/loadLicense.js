dac6cd8f5d6543b592168dafb2316b5198789d70
Sergey <serhii.orlivskyi@gmail.com>
2023-11-09 14:57:22 +0100

    Add CEDALO_LICENSE_KEY and CEDALO_LICENSE_FILE

--- backend/src/license/utils/loadLicense.js
+++ backend/src/license/utils/loadLicense.js
@@ -6,15 +6,22 @@ const readFile = require('./readFile');
 
 const loadPublicKey = () =>
 	readFile(process.env.CEDALO_MC_LICENSE_PUBLIC_KEY_PATH || path.join(__dirname, 'config', 'public.pem'));
-const loadLicenseKey = () =>
-	readFile(process.env.CEDALO_MC_LICENSE_PATH || path.join(__dirname, 'config', 'license.lic'))
-	.then((key) =>
-		key.toString().trim()
-	)
-	.catch((error) => {
-		console.log('No license key found or provided.');
-		throw error;
-	})
+const loadLicenseKey = () => {
+	if (process.env.CEDALO_LICENSE_KEY) {
+		const licenseString = process.env.CEDALO_LICENSE_KEY;
+		return Promise.resolve(licenseString.trim());
+	} else {
+		return readFile(process.env.CEDALO_LICENSE_FILE || process.env.CEDALO_MC_LICENSE_PATH || path.join(__dirname, 'config', 'license.lic'))
+		.then((key) =>
+			key.toString().trim()
+		)
+		.catch((error) => {
+			console.log('No license key found or provided.');
+			throw error;
+		});
+	}
+}
+
 
 const loadLicense = async () => {
 	let license;
